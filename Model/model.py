6311c580c0487f021cf77417fac644c1c5595774
fullzer4 <gabrielpelizzaro@gmail.com>
2024-05-16 09:28:09 -0300

    new

--- Model/model.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torchvision.models as models
-
-resnet_model = models.resnet50(pretrained=True)
-
-input_example = torch.randn(1, 3, 224, 224)
-
-torch.onnx.export(resnet_model, input_example, "resnet50.onnx", export_params=True)
